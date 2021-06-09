5e3e0a16f573297319ac70931b7d7e9e6d8dd195
Davi Oliveira <davioliveira-dev@outlook.com>
2021-06-09 13:11:40 -0300

    :construction_worker: :sparkles: WIP: featuring simple login page on app

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,7 +1,19 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
+import { Button } from './components/Button'
+import { Card } from './components/Card'
+import { Input } from './components/Input'
+import { Title } from './components/Title'
 
-const App: FC = () => {
-  return <h1 className="title">Hello World!</h1>
+export const App: FC = () => {
+  const [isLoading, setIsLoading] = useState(false)
+  const handleClick = () => setIsLoading(!isLoading)
+
+  return (
+    <Card>
+      <Title>Login</Title>
+      <Input title="Email" type="email" name="email" inputMode="email" />
+      <Input title="Senha" type="password" name="password" inputMode="text" />
+      <Button title="Login" isLoading={isLoading} onClick={handleClick} />
+    </Card>
+  )
 }
-
-export { App }
