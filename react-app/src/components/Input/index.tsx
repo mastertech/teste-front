0ed7358863170bb5ca6a8be30cdeca9a7a35febb
Davi Oliveira <davioliveira-dev@outlook.com>
2021-06-10 22:41:12 -0300

    Refactor: :hammer: Refatorando e adicionando forward ref

--- react-app/src/components/Input/index.tsx
+++ react-app/src/components/Input/index.tsx
@@ -1,18 +1,22 @@
-import { FC, InputHTMLAttributes } from 'react'
+import { FC, forwardRef, HTMLProps, InputHTMLAttributes } from 'react'
 import './styles.scss'
 
-type InputProps = Omit<
-  InputHTMLAttributes<HTMLInputElement>,
-  'placeholder' | 'classname'
-> & {
+type InputProps = HTMLProps<HTMLInputElement> & {
   title: string
 }
 
-export const Input: FC<InputProps> = ({ title, ...props }) => {
-  return (
-    <div className="wrapper">
-      <input className="floating-input" placeholder={title} {...props} />
-      <span className="floating-label">{title}</span>
-    </div>
-  )
-}
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ title, ...props }, ref) => {
+    return (
+      <div className="wrapper">
+        <input
+          className="floating-input"
+          ref={ref}
+          placeholder={title}
+          {...props}
+        />
+        <span className="floating-label">{title}</span>
+      </div>
+    )
+  }
+)
