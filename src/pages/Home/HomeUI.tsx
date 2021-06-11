c28df605f7ba8d9a4dd2af5f2d65110e24931f90
luca <luca.mqf@gmail.com>
2021-06-11 20:39:31 -0300

    feat: creates component for text and icons in Home page

--- src/pages/Home/HomeUI.tsx
+++ src/pages/Home/HomeUI.tsx
@@ -1,6 +1,8 @@
 import { IUser } from '../../@types/IUser'
 import { Layout } from '../../components/Layout';
+import { FaEnvelope, FaCalendar, FaMapPin, FaVenusMars } from 'react-icons/fa';
 import '../../styles/pages/Home.css';
+import { TextIcon } from '../../components/Home/TextIcon';
 
 type HomeProps = {
   user: IUser;
@@ -13,10 +15,10 @@ export const HomeUI: React.FC<HomeProps> = ({ user, logout }) => (
       <div className="user_info_container">
         <h1>{ user.name }</h1>
 
-        <p>{ user.email }</p>
-        <p>{ user.birthday }</p>
-        <p>{ user.state }</p>
-        <p>{ user.gender }</p>
+        <TextIcon Icon={FaEnvelope} text={user.email} />
+        <TextIcon Icon={FaCalendar} text={user.birthday} />
+        <TextIcon Icon={FaMapPin} text={user.state} />
+        <TextIcon Icon={FaVenusMars} text={user.gender} />
       </div>
 
       <img src={user.avatar} />
