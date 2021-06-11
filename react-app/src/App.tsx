ab2ae5fe7acea5b855a03aa3e8af5ce6aecd486a
Davi Oliveira <davioliveira-dev@outlook.com>
2021-06-11 01:07:07 -0300

    Feat: :sparkles: add Snackbar provider

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,15 +1,18 @@
 import { FC } from 'react'
-import { AuthProvider } from './context/AuthContext'
 import { Router } from 'react-router-dom'
 import { history } from './helpers/history'
 import { Routes } from './routes'
+import { AuthProvider } from './context/AuthContext'
+import { SnackBarProvider } from './context/SnackBarContext'
 
 export const App: FC = () => {
   return (
     <AuthProvider>
-      <Router history={history}>
-        <Routes />
-      </Router>
+      <SnackBarProvider>
+        <Router history={history}>
+          <Routes />
+        </Router>
+      </SnackBarProvider>
     </AuthProvider>
   )
 }
